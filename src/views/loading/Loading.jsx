5ccf8462ef5781aed09635a19de45333c22bf895
Ryan Tillaman <37836505+yellow-cyber@users.noreply.github.com>
2020-11-25 22:26:18 +0800

    loading ui

--- src/views/loading/Loading.jsx
+++ src/views/loading/Loading.jsx
@@ -3,7 +3,7 @@ import Spinner from "react-spinners/RingLoader";
 
 const Loading = ({ desc }) => {
   return (
-    <div className="flex  flex-col items-center text-white justify-center min-h-screen bg-blue-500">
+    <div className="flex  flex-col items-center text-white justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600">
       <Spinner color={"#fff"} size={150} />
       <p className="mt-2">{desc}</p>
     </div>
